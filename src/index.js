c292f870eb2b1d8f6dd60ca3830f85a28ee75456
lolimay <lolimay@lolimay.cn>
2021-01-23 23:20:17 +0800

    Move TM script into a seperate file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+// ==UserScript==
+// @name         Wolai Poet Theme
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  An elegant theme for wolai
+// @author       Shiqi Mei
+// @match        https://www.wolai.com/*
+// @grant        none
+// ==/UserScript==
+
 import './index.scss';
+
+(function () {
+    'use strict';
+
+    const styleSheet = document.createElement('style');
+    styleSheet.textContent = `
+html body {
+  background: green;
+}
+`.trim();
+    document.body.append(styleSheet);
+})();
