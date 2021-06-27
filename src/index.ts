039530b59687e20ed46e245713f3c97482637e8a
梅世祺 <t_shiqimei@kugou.gz>
2021-06-27 22:07:45 +0800

    【特性】自动展开更多

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import './index.scss';
+import './index.scss'
 
-import { Keyboard } from './keyboard';
-import { shortcuts } from './shortcut';
+import { Keyboard } from './keyboard'
+import { shortcuts } from './shortcut'
 
+// 按键重映射
 window.addEventListener('keydown', ev => {
     if (ev instanceof KeyboardEvent) {
         for (const { from, to } of shortcuts) {
             if (['Control', 'Meta', 'Shift', 'Alt'].includes(ev.key)) {
-                ev.preventDefault();
-                return ev.stopImmediatePropagation();
+                ev.preventDefault()
+                return ev.stopImmediatePropagation()
             }
-            console.log(ev);
 
             if (
                 from.ctrlKey === ev.ctrlKey &&
@@ -21,9 +21,15 @@ window.addEventListener('keydown', ev => {
             ) {
                 Keyboard.trigger(ev.target, to)
 
-                ev.preventDefault();
-                return ev.stopImmediatePropagation();
+                ev.preventDefault()
+                return ev.stopImmediatePropagation()
             }
         }
     }
-}, { capture: true });
+}, { capture: true })
+
+// 自动点击加载更多 5 次
+setTimeout(() => {
+    const loadMoreButton = document.querySelector('#private-tree nav>div:last-child') as HTMLDivElement
+    for (let i = 0; i < 5; i++) loadMoreButton.click()
+}, 500)
